8cd0cc3f03f3d09f6998af4c6440c95c0d88067d
劉軒宏 <3b032110@gm.student.ncut.edu.tw>
2023-12-06 00:14:16 +0800

    Present the content of a specific order to the user

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -41,6 +41,8 @@ class OrderController extends Controller
     public function show(Order $order)
     {
         //
+        $data = $order->orderItems()->get();
+        return view('orders.show',['order_items'=> $data]);
     }
 
     /**
